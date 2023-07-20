d143671c12ad447c14cd1fa8bb1bc82f9025af43
maxhack1337 <91089748+maxhack1337@users.noreply.github.com>
2023-07-20 12:31:28 +0300

    Delete starmouse.js

--- starmouse.js
+++
@@ -1,6 +0,0 @@
-setTimeout(function() {
-const el1ement = document.querySelector('div.ui_actions_menu_wrap._ui_menu_wrap.im-page--dialogs-call-wrap');
-el1ement.removeAttribute('onmouseover');
-el1ement.removeAttribute('onmouseout');
-console.log("Star executed!")
-},10000)
