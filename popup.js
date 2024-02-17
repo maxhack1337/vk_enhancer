362ad3e24b3f0a8d2879882f4709d2ff624d222b
Max <91089748+maxhack1337@users.noreply.github.com>
2024-02-17 21:39:02 +0300

    2.7.1.1 Snapshot

--- popup.js
+++ popup.js
@@ -154,10 +154,10 @@ fetch(url1)
 	var ver1 = document.getElementById('version');
 	const styleElement = document.createElement("style");
 	styleElement.id = "version";
-	styleElement.innerHTML = "#version::after{content:'Версия "+version+" GG VK'}";
+	styleElement.innerHTML = "#version::after{content:'Версия "+version+" SS'}";
 	document.head.appendChild(styleElement);
 	
-		if (version != "2.7.1")
+		if (version != "2.7.1.1")
 		{
 			var dialog = document.getElementById('updateAvailable');
 			dialog.style.display = 'block';
