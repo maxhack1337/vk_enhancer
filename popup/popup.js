16965832fbe8876ab39b9b0489d0350e9552bd88
Max <91089748+maxhack1337@users.noreply.github.com>
2024-05-08 17:33:25 +0300

    3.8 Update

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,4 @@
-console.log('Версия 3.7 Release');
+console.log('Версия 3.8 Release');
 var accentC = document.getElementById('oldaccent');
 var msgreact = document.getElementById('messagereactions');
 var recentgroups = document.getElementById('recentgroups');
