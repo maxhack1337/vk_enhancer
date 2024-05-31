a5c2d25108321e3806dd6ed91bb121a28efc8894
Max <91089748+maxhack1337@users.noreply.github.com>
2024-05-31 15:21:07 +0300

    3.11.3 Update

--- js/main.js
+++ js/main.js
@@ -92,6 +92,9 @@ var secretFunctions = false;
 let ajax_replaced = null;
 window.urls = null;
 
+window.vkenh = {};
+window.vkenh.setEnglishMusic = 0;
+
 deferredCallback(
   async (_vk) => {
     await getUserDataLocalStoragePhoto(vk.id);
@@ -352,6 +355,39 @@ window.addEventListener("message", async (event) => {
 document.arrive(".OwnerPageName__icons", { existing: true }, function (e) {
   updateUsers();
 });
+///РЕДАКТОР ГРАФФИТИ В КОММЕНТАХ///
+document.arrive(".ms_items_more._more_items", { existing: true }, function (e) {
+	let grafelem = document.createElement('a');
+	grafelem.classList.add('ms_item');
+	grafelem.classList.add('ms_item_graf');
+	grafelem.innerHTML = `<span class="MediaSelector__mediaIcon"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 20 20"><path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M6.66738 12.4934C5.66072 12.4934 4.89771 13.2639 4.85288 14.219C4.78943 15.5707 4.14574 16.3782 3.56758 16.8697C4.09576 17.0241 4.83625 17.0816 5.79412 16.7965C7.69684 16.2301 8.48106 15.0732 8.48106 14.1836C8.48106 13.2865 7.70617 12.4934 6.66738 12.4934ZM3.35859 14.1483C3.44019 12.4098 4.84452 10.9918 6.66738 10.9918C8.4581 10.9918 9.977 12.3845 9.977 14.1836C9.977 15.9903 8.47815 17.5638 6.21942 18.2361C4.06158 18.8784 2.57907 18.2114 1.87062 17.688C1.78649 17.6258 1.66019 17.5141 1.57758 17.3353C1.48322 17.131 1.47952 16.9126 1.54181 16.7213C1.69148 16.2618 2.17858 16.0626 2.52898 15.7829C2.9113 15.4778 3.31759 15.0219 3.35859 14.1483Z"></path><path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M15.7019 2.00141C16.312 1.36368 17.3109 1.32806 17.9631 1.93024C18.6094 2.527 18.68 3.5332 18.1327 4.21788L12.0531 11.716C11.7923 12.0376 11.3212 12.0861 11.0008 11.8244C10.6804 11.5626 10.6321 11.0897 10.8928 10.7681L16.9669 3.27699C17.0242 3.20385 17.0127 3.09301 16.9503 3.0354C16.8833 2.97354 16.8169 3.009 16.7482 3.07474L9.74543 9.7831C9.44658 10.0694 8.97312 10.0583 8.68792 9.75831C8.40272 9.45834 8.41378 8.98309 8.71263 8.69681L15.7019 2.00141Z"></path></svg></span>${getLang("mail_added_graffiti")} (editor)`;
+	grafelem.setAttribute(`onclick`,`showBox("al_wall.php", {act: "canvas_draw_box"}, {stat: [window.jsc("web/graffiti_new.js")],cache: 1,onDone: (e,t)=>{window.Graffiti.initDrawBox(e, t)}})`);
+	e.appendChild(grafelem);
+});
+///КОНЕЦ РЕДАКТОРА ГРАФФИТИ В КОММЕНТАХ///
+///ПЕРЕХОД К ПЕРВОМУ СООБЩ БЕСЕДЫ///
+document.arrive(".ConvoHeader__controls", { existing: true }, async function (e) {
+  let upToButton = document.createElement('button');
+  upToButton.classList.add('ConvoHeader__action');
+  upToButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+<path d="M13 19C13 19.5523 12.5523 20 12 20C11.4477 20 11 19.5523 11 19L11 7.41421L6.70711 11.7071C6.31658 12.0976 5.68342 12.0976 5.29289 11.7071C4.90237 11.3166 4.90237 10.6834 5.29289 10.2929L11.2929 4.29289C11.6834 3.90237 12.3166 3.90237 12.7071 4.29289L18.7071 10.2929C19.0976 10.6834 19.0976 11.3166 18.7071 11.7071C18.3166 12.0976 17.6834 12.0976 17.2929 11.7071L13 7.41421L13 19Z" fill="currentColor"/>
+</svg>`;
+  let cmid;
+  try {
+    let ThisUrl = new URL(window.location.href);
+	let match1 = ThisUrl.pathname.match(/\/(\d+)$/);
+	let reoo = await vkApi.api('messages.getHistory',{count:1,peer_id:match1[1],rev:1});
+	cmid = reoo.items[0].conversation_message_id;
+  }
+  catch(error) {
+	cmid = 1;  
+  }
+  upToButton.setAttribute('onclick',`nav.go("${window.location.href}?cmid=${cmid}")`);
+  try{
+  e.prepend(upToButton); }
+  catch(error){}
+});
+///КОНЕЦ ПЕРЕХОДА К ПЕРВОМУ СООБЩ БЕСЕДЫ///
 ///ПОСТЕРЫ///
 document.arrive("#page_add_media > .media_selector", { existing: true }, function (e) {
   let posters = document.createElement('a');
@@ -424,7 +460,7 @@ deferredCallback(
           switch (n) {
             case 10002: {
               const [, t, n, o] = e;
-              if (n) {
+              if (n > 10000) {
                 document.querySelectorAll(".ConvoHistory__messageBlock").forEach(se => {
                   if (getMessagePropsMin(se) == e[1]) {
                     se.classList.add("vkEnhancerDeletedMessageMain");
@@ -838,7 +874,13 @@ function handleDownloadButton(e) {
   const bar = e.target.parentNode.querySelector(".bar > .progress-bar");
   const id = getValidID(e.target);
   const Orig = getDownloadName(e.target);
-  const Cyr = parseCyr(Orig) ? parseCyr(Orig) : Orig;
+  const Orig2 = getDownloadName1(e.target);
+  let Cyr = Orig;
+  let Cyr1 = Orig2;
+  if(window.vkenh.setEnglishMusic == 1) {
+	Cyr = parseCyr(Orig) ? parseCyr(Orig) : Orig;
+	Cyr1 = parseCyr(Orig2) ? parseCyr(Orig2) : Orig2;
+	  }
   fetch("https://vk.com/al_audio.php?act=reload_audios", {
     headers: {
       "content-type": "application/x-www-form-urlencoded",
@@ -852,7 +894,7 @@ function handleDownloadButton(e) {
     .then((e) => e.json())
     .then((e) => {
       let url = _o(e.payload[1][0][0][2]);
-      console.log(Download_Fucking_Stream(url, Cyr, bar));
+      console.log(Download_Fucking_Stream(url, Cyr + " - " + Cyr1, bar));
     });
 }
 let getValidID = (elem) => {
@@ -861,6 +903,10 @@ let getValidID = (elem) => {
 
 let getDownloadName = (elem) => {
   return `${JSON.parse(elem.closest('.audio_row').dataset.audio)[3]}`;
+};
+
+let getDownloadName1 = (elem) => {
+  return `${JSON.parse(elem.closest('.audio_row').dataset.audio)[4]}`;
 };
 
 function Download_Fucking_Stream(url, name, elem) {
